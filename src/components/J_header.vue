df5e4b29877ba077c2f7948477542493cdb307a8
bubbly <bubblyface_sun@outlook.com>
2017-12-22 11:27:42 +0800

    common

---
+++ src/components/J_header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="J_header">
+      <header class="index-head">
+        
+      </header>
+  </div>
+</template>
+
+<style>
+.index-head {
+    position: fixed;
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    background-color: #f63;
+    color: #fff;
+    padding: 0 10px;
+    text-align: center;
+    display: -webkit-box;
+    z-index: 305;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+}
+</style>
+    
+<script>
+export default {
+  
+}
+</script>
